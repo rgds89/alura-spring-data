48376369217ef5c3063ac2e8a0f339e928f6f7ef
Roger Silva <rogersilvati@gmail.com>
2023-01-19 15:17:02 -0300

    first commit

---
+++ src/test/java/br/com/alura/springdata/AluraSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
